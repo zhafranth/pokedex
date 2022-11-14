9df5df17a9d455820c1399cd737c269fb5b33ca5
zhafranth <zhafranthGgmail.com>
2022-11-15 00:07:03 +0700

    feat(home): add content home

--- containers/Home/Home.tsx
+++ containers/Home/Home.tsx
@@ -1,12 +1,41 @@
+import { Colors } from "@constant/Color";
 import dynamic from "next/dynamic";
+import { Wrapper } from "./_components";
+import Image from "next/image";
+import ImageContent from "assets/home-content.png";
 
 const Button = dynamic(() => import("components/atoms/Button"));
+const Container = dynamic(() => import("@mui/material/Container"));
+const Typography = dynamic(() => import("@mui/material/Typography"));
 
 const Home = () => {
   return (
-    <div>
-      <Button>Check PokèDex</Button>
-    </div>
+    <Container>
+      <Wrapper>
+        <div className="content">
+          <Typography
+            variant="h4"
+            fontFamily={"inherit"}
+            fontWeight="700"
+            color={Colors.neutral}
+            fontSize="3.25rem"
+            marginBottom="1rem"
+          >
+            All the Pokémon data youll ever need in one place!
+          </Typography>
+          <Typography
+            paragraph
+            fontFamily={"inherit"}
+            marginBottom="2rem"
+            fontSize="1.25rem"
+          >
+            Thousands of data compiled into one place
+          </Typography>
+          <Button>Check PokèDex</Button>
+        </div>
+        <Image src={ImageContent} alt="pokemon group" />
+      </Wrapper>
+    </Container>
   );
 };
 
---
+++ containers/Home/_components.tsx
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  column-gap: 2rem;
+  .content {
+    width: 45%;
+  }
+`;
