134a86c9e9feebd271c30c230ad35fe6c48cd447
zhafranth <zhafranthGgmail.com>
2022-11-17 00:39:51 +0700

    refactor(pokemon-detail): add section detail component

---
+++ components/molecules/SectionDetail/SectionDetail.tsx
@@ -0,0 +1,33 @@
+import dynamic from "next/dynamic";
+
+const Box = dynamic(() => import("@mui/material/Box"));
+const Text = dynamic(() => import("@mui/material/Typography"));
+
+interface SectionDetailProperties {
+  title: string;
+  children: React.ReactNode;
+}
+
+const SectionDetail: React.FC<SectionDetailProperties> = ({
+  title,
+  children,
+}) => {
+  return (
+    <Box marginTop="5rem">
+      <Text fontSize="1.25rem" fontWeight="700">
+        {title}
+      </Text>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        columnGap="1rem"
+        marginTop="2rem"
+        flexWrap="wrap"
+      >
+        {children}
+      </Box>
+    </Box>
+  );
+};
+
+export default SectionDetail;
--- containers/PokemonDetail/PokemonDetail.tsx
+++ containers/PokemonDetail/PokemonDetail.tsx
@@ -8,6 +8,9 @@ const Container = dynamic(() => import("@mui/material/Container"));
 const Box = dynamic(() => import("@mui/material/Box"));
 const Text = dynamic(() => import("@mui/material/Typography"));
 const PokemonInfo = dynamic(() => import("@components/organisms/pokemonInfo"));
+const SectionDetail = dynamic(
+  () => import("@components/molecules/SectionDetail")
+);
 
 const PokemonDetail = () => {
   const router = useRouter();
@@ -22,37 +25,26 @@ const PokemonDetail = () => {
   return (
     <Container>
       <PokemonInfo data={detailPokemon} loading={isLoading} />
-      <Box marginTop="5rem">
-        <Text fontSize="1.25rem" fontWeight="700">
-          Other Images
-        </Text>
-        <Box
-          display="flex"
-          justifyContent="space-between"
-          columnGap="1rem"
-          marginTop="2rem"
-          flexWrap="wrap"
-        >
-          {stats?.map((item, index) => (
-            <StatsWrapper
-              key={`${item.stat.name}-${index}`}
-              color={StatsColor[item.stat.name]}
+      <SectionDetail title="Stats">
+        {stats?.map((item, index) => (
+          <StatsWrapper
+            key={`${item.stat.name}-${index}`}
+            color={StatsColor[item.stat.name]}
+          >
+            <Text
+              fontSize="3rem"
+              fontWeight="700"
+              variant="h4"
+              style={{ color: StatsColor[item.stat.name] }}
             >
-              <Text
-                fontSize="3rem"
-                fontWeight="700"
-                variant="h4"
-                style={{ color: StatsColor[item.stat.name] }}
-              >
-                {item.base_stat}
-              </Text>
-              <Text paragraph fontSize="0.8rem" fontWeight={500}>
-                {item.stat.name.replace("-", " ")}
-              </Text>
-            </StatsWrapper>
-          ))}
-        </Box>
-      </Box>
+              {item.base_stat}
+            </Text>
+            <Text paragraph fontSize="0.8rem" fontWeight={500}>
+              {item.stat.name.replace("-", " ")}
+            </Text>
+          </StatsWrapper>
+        ))}
+      </SectionDetail>
     </Container>
   );
 };
